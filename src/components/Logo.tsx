fcac9181a463411671695b55768a07de8df8ec56
Tatiana Trofimova <nerpassevera@gmail.com>
2025-01-21 15:01:34 -0800

    Removes some duplicates

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,13 +1,12 @@
 import { Link } from 'react-router-dom';
-import { Armchair } from 'lucide-react';
+// import { Armchair } from 'lucide-react';
 
 function Logo() {
   return (
     <Link
       to='/'
-      className='hidden lg:flex justify-center items-center bg-primary p-2 rounded-lg text-white '
     >
-      <Armchair className='w-8 h-8' />
+      <img src="../public/logo_full.png" className='h-8' alt="" />
     </Link>
   );
 }
