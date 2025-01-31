0ed7b328b470308a2092029b75e3c9c3e43f829c
Tatiana Trofimova <nerpassevera@gmail.com>
2025-01-30 17:43:52 -0800

    Single Product page connected to BE

--- src/pages/SingleProduct.tsx
+++ src/pages/SingleProduct.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import {
   customFetch,
   formatAsDollars,
-  type SingleProductResponse,
+  type Product,
   type CartItem
 } from '../utils';
 import { useState } from 'react';
@@ -15,40 +15,40 @@ import { Mode } from '../components/SelectProductAmount';
 import { useAppDispatch } from '../lib/hooks';
 import { addItem } from '../features/cart/cartSlice';
 
-export const loader: LoaderFunction = async ({params}): Promise<SingleProductResponse> =>  {
-  const response = await customFetch<SingleProductResponse>(
+export const loader: LoaderFunction = async ({ params }): Promise<Product> => {
+
+  const response = await customFetch<Product>(
     `/products/${params.id}`
   );
-  return { ...response.data };
+
+  return response.data
 };
 
 function SingleProduct() {
-  const loaderData = useLoaderData() as SingleProductResponse | undefined;
-  const product = loaderData?.data;
-  
-  if (!product) {
-    return <p>Loading...</p>; // Заглушка, пока нет данных
-  }  console.log('product >>> ', product);
-  
-  const { image, title, price, description } =
-    product.attributes;
+  const loaderData = useLoaderData() as Product;
+
+  const product = loaderData;
+
+  const { image_url, name, price, description } =
+    product;
+
   const dollarsAmount = formatAsDollars(price);
   const [amount, setAmount] = useState(1);
 
-  const dispatch = useAppDispatch();
+  // const dispatch = useAppDispatch();
 
-  const cartProduct: CartItem = {
-    cartID: `${product.id}`,
-    productID: product.id,
-    image,
-    title,
-    price,
-    amount,
-  };
+  // const cartProduct: CartItem = {
+  //   cartID: `${product.id}`,
+  //   productID: product.id,
+  //   image_url,
+  //   name,
+  //   price,
+  //   amount,
+  // };
 
-  const addToCart = () => {
-    dispatch(addItem(cartProduct));
-  };
+  // const addToCart = () => {
+  //   dispatch(addItem(cartProduct));
+  // };
 
   return <section>
     <div className='flex gap-x-2 h-6 items-center'>
@@ -64,13 +64,13 @@ function SingleProduct() {
     <div className='mt-6 grid gap-y-8 lg:grid-cols-2  lg:gap-x-16'>
       {/* IMAGE FIRST COL*/}
       <img
-        src={image}
-        alt={title}
+        src={image_url}
+        alt={name}
         className='w-96 h-96 object-cover rounded-lg lg:w-full'
       />
       {/* PRODUCT INFO SECOND COL */}
       <div>
-        <h1 className='capitalize text-3xl font-bold'>{title}</h1>
+        <h1 className='capitalize text-3xl font-bold'>{name}</h1>
         {/* <h4 className='text-xl mt-2'>{company}</h4> */}
         <p className='mt-3 text-md bg-muted inline-block p-2 rounded-md'>
           {dollarsAmount}
@@ -80,7 +80,7 @@ function SingleProduct() {
         <SelectProductAmount mode={Mode.SingleProduct} amount={amount} setAmount={setAmount} />
 
         {/* CART BUTTON */}
-        <Button size='lg' className='mt-10' onClick={addToCart}>
+        <Button size='lg' className='mt-10' onClick={() => console.log('pfff')/* addToCart */}>
           Add to bag
         </Button>
       </div>
@@ -88,105 +88,3 @@ function SingleProduct() {
   </section>
 };
 export default SingleProduct;
-
-
-
-
-
-// import { useLoaderData } from 'react-router-dom';
-// import { Link } from 'react-router-dom';
-// import {
-//   customFetch,
-//   formatAsDollars,
-//   type SingleProductResponse,
-//   type CartItem
-// } from '../utils';
-// import { useState } from 'react';
-// import { Button } from "../components/ui/button";
-// import { Separator } from '../components/ui/separator';
-// import { SelectProductAmount, SelectProductColor } from '../components';
-// import { type LoaderFunction } from 'react-router-dom';
-// import { Mode } from '../components/SelectProductAmount';
-// import { useAppDispatch } from '../lib/hooks';
-// import { addItem } from '../features/cart/cartSlice';
-
-// export const loader: LoaderFunction = async ({params}): Promise<SingleProductResponse> =>  {
-//   const response = await customFetch<SingleProductResponse>(
-//     `/products/${params.id}`
-//   );
-//   return { ...response.data };
-// };
-
-
-// function SingleProduct() {
-//   const { data: product } = useLoaderData() as SingleProductResponse;
-//   const { image, title, price, description, colors, company } =
-//     product.attributes;
-//   const dollarsAmount = formatAsDollars(price);
-//   // const [productColor, setProductColor] = useState(colors[0]);
-//   const [amount, setAmount] = useState(1);
-
-//   const dispatch = useAppDispatch();
-
-//   const cartProduct: CartItem = {
-//     cartID: product.id + productColor,
-//     productID: product.id,
-//     image,
-//     title,
-//     price,
-//     amount,
-//     // productColor,
-//     company,
-//   };
-
-//   const addToCart = () => {
-//     dispatch(addItem(cartProduct));
-//   };
-
-//   return <section>
-//     <div className='flex gap-x-2 h-6 items-center'>
-//       <Button asChild variant='link' size='sm'>
-//         <Link to='/'>Home</Link>
-//       </Button>
-//       <Separator orientation='vertical' />
-//       <Button asChild variant='link' size='sm'>
-//         <Link to='/products'>Products</Link>
-//       </Button>
-//     </div>
-//     {/* PRODUCT */}
-//     <div className='mt-6 grid gap-y-8 lg:grid-cols-2  lg:gap-x-16'>
-//       {/* IMAGE FIRST COL*/}
-//       <img
-//         src={image}
-//         alt={title}
-//         className='w-96 h-96 object-cover rounded-lg lg:w-full'
-//       />
-//       {/* PRODUCT INFO SECOND COL */}
-//       <div>
-//         <h1 className='capitalize text-3xl font-bold'>{title}</h1>
-//         <h4 className='text-xl mt-2'>{company}</h4>
-//         <p className='mt-3 text-md bg-muted inline-block p-2 rounded-md'>
-//           {dollarsAmount}
-//         </p>
-//         <p className='mt-6 leading-8'>{description}</p>
-//         {/* COLORS
-//         <SelectProductColor
-//           colors={colors}
-//           productColor={productColor}
-//           setProductColor={setProductColor} */}
-//         {/* /> */}
-//         {/* AMOUNT */}
-//         <SelectProductAmount mode={Mode.SingleProduct} amount={amount} setAmount={setAmount} />
-
-
-//         {/* CART BUTTON */}
-//         <Button size='lg' className='mt-10' onClick={addToCart}>
-//           Add to bag
-//         </Button>
-//       </div>
-//     </div>
-//   </section>
-// };
-// export default SingleProduct;
-
-
